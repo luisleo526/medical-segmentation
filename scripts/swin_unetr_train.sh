3fcbda27fb2e1a30e23014a99e0713ba94c47edc
Luis <luisleo52655@gmail.com>
2023-11-18 00:33:19 +0800

    debug
    Signed-off-by: Luis <luisleo52655@gmail.com>

--- scripts/swin_unetr_train.sh
+++ scripts/swin_unetr_train.sh
@@ -1,5 +1,5 @@
 accelerate launch --num_cpu_threads_per_process 8 train.py \
 data.root=/workspace/dataset/Task08_HepaticVessel model=swin_unetr \
-model.batch_size.train=2 model.batch_size.val=16 model.accumulation_steps=1 \
-model.optimizer.params.lr=1e-2 \
+model.batch_size.train=2 model.batch_size.val=16 model.accumulation_steps=2 \
+model.optimizer.params.lr=3e-4 \
 val_freq=10 save_tag=swin_unetr_task08
