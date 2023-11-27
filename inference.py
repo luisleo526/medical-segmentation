cef76300ff681f95b4efebaf5bf18bf6e42f6461
Luis <luisleo52655@gmail.com>
2023-11-27 19:50:14 +0800

    debug
    Signed-off-by: Luis <luisleo52655@gmail.com>

--- inference.py
+++ inference.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from glob import glob
 from pathlib import Path
 
+import cv2
 import numpy as np
 import torch
 from monai.inferers import sliding_window_inference
@@ -12,6 +13,37 @@ from uvw import RectilinearGrid, DataArray
 import wandb
 from dataset import get_transforms
 from utils import initiate, extract_elements
+
+
+def overlay(image, mask, color, alpha, resize=None):
+    """Combines image and its segmentation mask into a single image.
+    https://www.kaggle.com/code/purplejester/showing-samples-with-segmentation-mask-overlay
+
+    Params:
+        image: Training image. np.ndarray,
+        mask: Segmentation mask. np.ndarray,
+        color: Color for segmentation mask rendering.  tuple[int, int, int] = (255, 0, 0)
+        alpha: Segmentation mask's transparency. float = 0.5,
+        resize: If provided, both image and its mask are resized before blending them together.
+        tuple[int, int] = (1024, 1024))
+
+    Returns:
+        image_combined: The combined image. np.ndarray
+
+    """
+    color = color[::-1]
+    colored_mask = np.expand_dims(mask, 0).repeat(3, axis=0)
+    colored_mask = np.moveaxis(colored_mask, 0, -1)
+    masked = np.ma.MaskedArray(image, mask=colored_mask, fill_value=color)
+    image_overlay = masked.filled()
+
+    if resize is not None:
+        image = cv2.resize(image.transpose(1, 2, 0), resize)
+        image_overlay = cv2.resize(image_overlay.transpose(1, 2, 0), resize)
+
+    image_combined = cv2.addWeighted(image, 1 - alpha, image_overlay, alpha, 0)
+
+    return image_combined
 
 
 def parse_args():
@@ -54,10 +86,18 @@ if __name__ == '__main__':
             start_idx = min(uniques)
             end_idx = max(uniques)
 
-            # for z in extract_elements(range(start_idx, end_idx + 1), args.samples):
-            #     img = image_tensor.squeeze(0).squeeze(0).cpu().numpy()[..., z]
-            #     H, W = img.shape
-            #     mask =
+            for z in extract_elements(range(start_idx, end_idx + 1), args.samples):
+                img = image_tensor.squeeze(0).squeeze(0).cpu().numpy()[..., z]
+                img = np.stack([img, img, img], axis=-1)
+                lab = p_label[..., z]
+                H, W = img.shape
+
+                img = overlay(img, lab == 1, (255, 0, 0), 0.5)
+                img = overlay(img, lab == 2, (0, 255, 0), 0.5)
+
+                filename = Path(image).stem.split('.')[0] + '.png'
+                filename = args.output / filename
+                cv2.imwrite(str(filename), img)
 
         nx, ny, nz = p_label.shape
 
