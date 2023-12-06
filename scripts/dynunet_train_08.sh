f072badb5d94090f7f571a6685bde679d9c0da9c
Luis <luisleo52655@gmail.com>
2023-12-06 10:14:42 +0800

    debug
    Signed-off-by: Luis <luisleo52655@gmail.com>

--- scripts/dynunet_train_08.sh
+++ scripts/dynunet_train_08.sh
@@ -1,6 +1,6 @@
 accelerate launch --num_cpu_threads_per_process 16 train.py \
 data.root=/workspace/dataset/Task08_HepaticVessel \
-model=dynunet optimizer=adam scheduler=warmup_cosine \
+model=dynunet optimizer=sgd scheduler=warmup_cosine \
 optimizer.params.lr=1.0e-2 \
 batch_size.train=16 batch_size.val=24 accumulation_steps=1 \
 val_freq=10 save_tag=dynunet_task08
--- scripts/swin_unetr_train_08.sh
+++ scripts/swin_unetr_train_08.sh
@@ -1,6 +1,6 @@
 accelerate launch --num_cpu_threads_per_process 16 train.py \
 data.root=/workspace/dataset/Task08_HepaticVessel \
-model=dynunet optimizer=adam scheduler=warmup_cosine \
+model=dynunet optimizer=sgd scheduler=warmup_cosine \
 optimizer.params.lr=3.0e-4 \
 batch_size.train=2 batch_size.val=16 accumulation_steps=1 \
 val_freq=10 save_tag=dynunet_task08
