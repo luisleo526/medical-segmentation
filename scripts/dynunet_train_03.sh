e29cf85d0d091fd3484929828f6326064172f363
luisleo.nhri <luisleo526@yahoo.com>
2023-12-14 23:15:13 +0000

    update from nhri

--- scripts/dynunet_train_03.sh
+++ scripts/dynunet_train_03.sh
@@ -1,7 +1,8 @@
-accelerate launch --num_cpu_threads_per_process 16 train.py num_workers=16 \
+accelerate launch --num_cpu_threads_per_process 16 train.py \
+dataset.params.num_workers=16 num_workers=16 buffer_size=16 \
 data=task3 data.root=/workspace/dataset/Task03_Liver \
 name=medical_decathlon_task3 \
 model=dynunet optimizer=sgd scheduler=warmup_cosine \
 optimizer.params.lr=1.0e-2 \
-batch_size.train=16 batch_size.val=24 accumulation_steps=1 \
+batch_size.train=2 batch_size.val=16 accumulation_steps=1 \
 val_freq=10 save_tag=dynunet_task03
