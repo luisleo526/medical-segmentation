eedabafff7d74320c397b0b5ab3d63164cefa331
HaoLiangWen <luisleo52655@gmail.com>
2023-12-22 20:05:28 +0800

    >> testing transform from dynunet pipeline

--- dataset/transform.py
+++ dataset/transform.py
@@ -1,25 +1,14 @@
 import numpy as np
 from monai.transforms import (CastToTyped, SpatialPadd, RandCropByPosNegLabeld, Compose, CropForegroundd,
-                              EnsureChannelFirstd, LoadImaged, RandFlipd, RandGaussianNoised, Spacingd,
-                              RandGaussianSmoothd,
-                              RandScaleIntensityd, ScaleIntensityRanged, NormalizeIntensityd,
-                              RandZoomd, ToTensord, EnsureTyped)
-from monai.transforms import MapTransform
-
-
-class ReadNumpyArray(MapTransform):
-    def __init__(self, keys):
-        super().__init__(keys)
-        self.to_tensor = ToTensord(keys=keys)
-
-    def __call__(self, data):
-        for key in self.keys:
-            data[key] = np.load(data[key])
-        return self.to_tensor(data)
+                              EnsureChannelFirstd, LoadImaged, RandFlipd, RandGaussianNoised, RandGaussianSmoothd,
+                              RandScaleIntensityd, RandZoomd, ToTensord, EnsureTyped)
+from monai.transforms import MapTransform, NormalizeIntensity, SpatialCrop
+from monai.transforms.utils import generate_spatial_bounding_box
+from skimage.transform import resize
 
 
 def get_transforms(mode, cfg):
-    # mode: train, val, test, selftrain
+    # mode: train, val, test
     if mode != "test":
         keys = ["image", "label"]
         spacing_mode = ("bilinear", "nearest")
@@ -33,28 +22,20 @@ def get_transforms(mode, cfg):
     load_transforms = [
         LoadImaged(keys=keys),
         EnsureChannelFirstd(keys=keys),
+        PreprocessAnisotropic(
+            keys=keys,
+            clip_values=cfg.data.clip_values,
+            pixdim=cfg.data.spacing,
+            normalize_values=cfg.data.normalize_values,
+            model_mode=mode,
+        ),
         ToTensord(keys=keys)
     ]
 
-    a_min = (cfg.data.clip_values[0] - cfg.data.normalize_values[0]) / cfg.data.normalize_values[1]
-    a_max = (cfg.data.clip_values[1] - cfg.data.normalize_values[0]) / cfg.data.normalize_values[1]
-
-    # 2. sampling (4)
-    sample_transforms = [
-        Spacingd(keys=keys, pixdim=cfg.data.spacing, mode=spacing_mode, align_corners=spacing_ac),
-        SpatialPadd(keys=keys, spatial_size=cfg.data.patch_size, mode='replicate'),
-        CropForegroundd(keys=keys, source_key="image", allow_smaller=False),
-        NormalizeIntensityd(keys="image",
-                            subtrahend=cfg.data.normalize_values[0],
-                            divisor=cfg.data.normalize_values[1]),
-        ScaleIntensityRanged(keys="image",
-                             a_min=a_min, a_max=a_max,
-                             b_min=0, b_max=1, clip=True)
-    ]
-
-    # 3. spatial transforms (9)
-    if mode == "train" or mode == "selftrain":
+    # 2. spatial transforms (9)
+    if mode == "train":
         augmentation = [
+            SpatialPadd(keys=["image", "label"], spatial_size=cfg.data.patch_size),
             RandCropByPosNegLabeld(
                 keys=["image", "label"],
                 label_key="label",
@@ -64,14 +45,13 @@ def get_transforms(mode, cfg):
                 num_samples=cfg.data.num_samples,
                 image_key="image",
             ),
-            SpatialPadd(keys=["image", "label"], spatial_size=cfg.data.patch_size),
             RandZoomd(
                 keys=["image", "label"],
-                min_zoom=0.8,
+                min_zoom=0.9,
                 max_zoom=1.2,
                 mode=("trilinear", "nearest"),
                 align_corners=(True, None),
-                prob=0.2,
+                prob=0.15,
             ),
             RandGaussianNoised(keys=["image"], std=0.01, prob=0.15),
             RandGaussianSmoothd(
@@ -87,9 +67,9 @@ def get_transforms(mode, cfg):
             RandFlipd(["image", "label"], spatial_axis=[2], prob=0.5),
         ]
     else:
-        augmentation = []
+        augmentation = [SpatialPadd(keys=["image"], spatial_size=cfg.data.patch_size)]
 
-    # 4. data casting (2)
+    # 3. data casting (2)
     if mode == 'test':
         data_casting = [
             CastToTyped(keys=["image"], dtype=(np.float32)),
@@ -101,18 +81,195 @@ def get_transforms(mode, cfg):
             EnsureTyped(keys=["image", "label"])
         ]
 
-    if mode != 'train':
-        data_casting = data_casting * 4
-
-    # Summary
-    # 　train: 4 + 4 + 9 + 2 = 19
-    # 　val: 4 + 4 + 2 = 10
-    # 　test: 4 + 4 + 2 = 10
-    # 　selftrain: １ + 9 + 2 = 12
-
-    all_transforms = load_transforms + sample_transforms + augmentation + data_casting
-
-    if mode == 'selftrain':
-        all_transforms = [ReadNumpyArray(keys=keys)] + augmentation + data_casting
+    all_transforms = load_transforms + augmentation + data_casting
 
     return Compose(all_transforms)
+
+
+def resample_image(image, shape, anisotrophy_flag):
+    resized_channels = []
+    if anisotrophy_flag:
+        for image_c in image:
+            resized_slices = []
+            for i in range(image_c.shape[-1]):
+                image_c_2d_slice = image_c[:, :, i]
+                image_c_2d_slice = resize(
+                    image_c_2d_slice,
+                    shape[:-1],
+                    order=3,
+                    mode="edge",
+                    cval=0,
+                    clip=True,
+                    anti_aliasing=False,
+                )
+                resized_slices.append(image_c_2d_slice)
+            resized = np.stack(resized_slices, axis=-1)
+            resized = resize(
+                resized,
+                shape,
+                order=0,
+                mode="constant",
+                cval=0,
+                clip=True,
+                anti_aliasing=False,
+            )
+            resized_channels.append(resized)
+    else:
+        for image_c in image:
+            resized = resize(
+                image_c,
+                shape,
+                order=3,
+                mode="edge",
+                cval=0,
+                clip=True,
+                anti_aliasing=False,
+            )
+            resized_channels.append(resized)
+    resized = np.stack(resized_channels, axis=0)
+    return resized
+
+
+def resample_label(label, shape, anisotrophy_flag):
+    reshaped = np.zeros(shape, dtype=np.uint8)
+    n_class = np.max(label)
+    if anisotrophy_flag:
+        shape_2d = shape[:-1]
+        depth = label.shape[-1]
+        reshaped_2d = np.zeros((*shape_2d, depth), dtype=np.uint8)
+
+        for class_ in range(1, int(n_class) + 1):
+            for depth_ in range(depth):
+                mask = label[0, :, :, depth_] == class_
+                resized_2d = resize(
+                    mask.astype(float),
+                    shape_2d,
+                    order=1,
+                    mode="edge",
+                    cval=0,
+                    clip=True,
+                    anti_aliasing=False,
+                )
+                reshaped_2d[:, :, depth_][resized_2d >= 0.5] = class_
+        for class_ in range(1, int(n_class) + 1):
+            mask = reshaped_2d == class_
+            resized = resize(
+                mask.astype(float),
+                shape,
+                order=0,
+                mode="constant",
+                cval=0,
+                clip=True,
+                anti_aliasing=False,
+            )
+            reshaped[resized >= 0.5] = class_
+    else:
+        for class_ in range(1, int(n_class) + 1):
+            mask = label[0] == class_
+            resized = resize(
+                mask.astype(float),
+                shape,
+                order=1,
+                mode="edge",
+                cval=0,
+                clip=True,
+                anti_aliasing=False,
+            )
+            reshaped[resized >= 0.5] = class_
+
+    reshaped = np.expand_dims(reshaped, 0)
+    return reshaped
+
+
+class PreprocessAnisotropic(MapTransform):
+    """
+    This transform class takes NNUNet's preprocessing method for reference.
+    That code is in:
+    https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunet/preprocessing/preprocessing.py
+
+    """
+
+    def __init__(
+            self,
+            keys,
+            clip_values,
+            pixdim,
+            normalize_values,
+            model_mode,
+    ) -> None:
+        super().__init__(keys)
+        self.keys = keys
+        self.low = clip_values[0]
+        self.high = clip_values[1]
+        self.target_spacing = pixdim
+        self.mean = normalize_values[0]
+        self.std = normalize_values[1]
+        self.training = False
+        self.crop_foreg = CropForegroundd(keys=["image", "label"], source_key="image", allow_missing_keys=True)
+        self.normalize_intensity = NormalizeIntensity(nonzero=True, channel_wise=True)
+        if model_mode in ["train"]:
+            self.training = True
+
+    def calculate_new_shape(self, spacing, shape):
+        spacing_ratio = np.array(spacing) / np.array(self.target_spacing)
+        new_shape = (spacing_ratio * np.array(shape)).astype(int).tolist()
+        return new_shape
+
+    def check_anisotrophy(self, spacing):
+        def check(spacing):
+            return np.max(spacing) / np.min(spacing) >= 3
+
+        return check(spacing) or check(self.target_spacing)
+
+    def __call__(self, data):
+        # load data
+        d = dict(data)
+        image = d["image"]
+
+        image_spacings = d["image_meta_dict"]["pixdim"][1:4].tolist()
+
+        if "label" in self.keys:
+            label = d["label"]
+            label[label < 0] = 0
+
+        if self.training:
+            # only task 04 does not be impacted
+            cropped_data = self.crop_foreg({"image": image, "label": label})
+            image, label = cropped_data["image"], cropped_data["label"]
+        else:
+            d["original_shape"] = np.array(image.shape[1:])
+            box_start, box_end = generate_spatial_bounding_box(image)
+            image = SpatialCrop(roi_start=box_start, roi_end=box_end)(image)
+            d["bbox"] = np.vstack([box_start, box_end])
+            d["crop_shape"] = np.array(image.shape[1:])
+
+        original_shape = image.shape[1:]
+        # calculate shape
+        resample_flag = False
+        anisotrophy_flag = False
+
+        image = image.numpy()
+        if self.target_spacing != image_spacings:
+            # resample
+            resample_flag = True
+            resample_shape = self.calculate_new_shape(image_spacings, original_shape)
+            anisotrophy_flag = self.check_anisotrophy(image_spacings)
+            image = resample_image(image, resample_shape, anisotrophy_flag)
+            if self.training:
+                label = resample_label(label, resample_shape, anisotrophy_flag)
+
+        d["resample_flag"] = resample_flag
+        d["anisotrophy_flag"] = anisotrophy_flag
+        # clip image for CT dataset
+        if self.low != 0 or self.high != 0:
+            image = np.clip(image, self.low, self.high)
+            image = (image - self.mean) / self.std
+        else:
+            image = self.normalize_intensity(image.copy())
+
+        d["image"] = image
+
+        if "label" in self.keys:
+            d["label"] = label
+
+        return d
